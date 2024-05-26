282297d4ace72694d5f111dbadcb27012a90dc9a
Andrey <145703493+Sparkell72@users.noreply.github.com>
2024-05-26 03:39:10 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,90 @@
+let tg = window.telegram.WebApp;
+
+tg.expend();
+
+tg.MainButton.textColor = "#FFFFFF"
+tg.MainButton.color = "#2cab37"
+
+let item = "";
+
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
+
+btn1.addEventListener("click", function(){
+    if (tg.MainButton.isVisible){
+        tg.MainButton.hide();
+    }
+    else{
+        tg.MainButton.setText("Вы выбрали товар 1!");
+        item = "1";
+        tg.MainButton.show();
+    }
+});
+btn2.addEventListener("click", function(){
+    if (tg.MainButton.isVisible){
+        tg.MainButton.hide();
+    }
+    else{
+        tg.MainButton.setText("Вы выбрали товар 1!");
+        item = "1";
+        tg.MainButton.show();
+    }
+});
+btn3.addEventListener("click", function(){
+    if (tg.MainButton.isVisible){
+        tg.MainButton.hide();
+    }
+    else{
+        tg.MainButton.setText("Вы выбрали товар 1!");
+        item = "1";
+        tg.MainButton.show();
+    }
+});
+btn4.addEventListener("click", function(){
+    if (tg.MainButton.isVisible){
+        tg.MainButton.hide();
+    }
+    else{
+        tg.MainButton.setText("Вы выбрали товар 1!");
+        item = "1";
+        tg.MainButton.show();
+    }
+});
+btn5.addEventListener("click", function(){
+    if (tg.MainButton.isVisible){
+        tg.MainButton.hide();
+    }
+    else{
+        tg.MainButton.setText("Вы выбрали товар 1!");
+        item = "1";
+        tg.MainButton.show();
+    }
+});
+btn6.addEventListener("click", function(){
+    if (tg.MainButton.isVisible){
+        tg.MainButton.hide();
+    }
+    else{
+        tg.MainButton.setText("Вы выбрали товар 1!");
+        item = "1";
+        tg.MainButton.show();
+    }
+});
+
+
+telegram.Webapp.onEvent("mainButtonClicked", function(){
+    tg.sedData(item);
+});
+
+let usercard = document.getElementById("usercard");
+
+let p = document.createElement("p")
+
+p.innerText = `${tg.initDataUsnafe.first_name}`
+`${tg.initDataUsnafe.last_name}`
+
+usercard.appendChild(p);
